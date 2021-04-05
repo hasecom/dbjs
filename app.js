7bce03c57f4ec729a3529615405acdd032544e0c
hasecom <hasetennisth@gmail.com>
2021-04-05 20:40:00 +0900

    update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json())
 
 app.set("view engine", "pug")
 app.get('/', (req, res) => { 
-  res.render("index",{title:"index",message:"hello a  there"}) 
+  res.render("index",{title:"index",message:"タスクビュー"}) 
 });
 
 
